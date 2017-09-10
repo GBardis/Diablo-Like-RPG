981b1dc17fdbb8862f7a876f2d121c60ab35b0dd
NValanos <nvvalanos@gmail.com>
2017-09-10 12:49:25 +0300

    till Part 6 imported

---
+++ Assets/Scripts/Systems/CameraFollow.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	[Header("Target")]
+	public Transform target;
+	[Header("Distances")]
+	public float distance = 5f;
+	public float minDistance = 1f;
+	public float maxDistance = 7f;
+	public Vector3 offset;
+	[Header("Speeds")]
+	public float smoothSpeed = 5f;
+	public float scrollSensitivity = 1;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () 
+	{
+		if (!target)
+		{
+			print("NO TARGET SET FOR THE CAMERA!!");
+			return;
+		}
+
+		float num = Input.GetAxis("Mouse ScrollWheel");
+		distance -= num * scrollSensitivity;
+		distance = Mathf.Clamp(distance, minDistance, maxDistance);
+
+		Vector3 pos = target.position + offset;
+		pos -= transform.forward * distance;
+
+		transform.position = Vector3.Lerp(transform.position, pos, smoothSpeed * Time.deltaTime);
+	}
+}
